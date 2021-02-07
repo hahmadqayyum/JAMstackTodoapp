28ea249ef06a929bcb4c786e158d69d0bf4c2446
hahmadqayyum <hahmadqayyum@gmail.com>
2021-02-07 10:21:51 -0800

    netlify identity and theme ui and gatsby setup

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Button, Container, Flex, Heading } from "theme-ui";
+import netlifyIdentity from "netlify-identity-widget";
+
+netlifyIdentity.init({});
 
 export default (props) => (
-  <div>
-    <h1>Hello Serverless</h1>
-  </div>
+  <Container>
+    <Flex sx={{ flexDirection: "column", padding: 3 }}>
+      <Heading as="h1">Hello Serverless</Heading>
+      <Button
+        sx={{
+          marginTop: 3,
+          outline: "none",
+        }}
+        onClick={() => {
+          netlifyIdentity.open();
+        }}
+      >
+        LogIn
+      </Button>
+    </Flex>
+  </Container>
 );

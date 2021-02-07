fdc552e69612ad7f5f2401328834c54860fcbcc9
hahmadqayyum <hahmadqayyum@gmail.com>
2021-02-07 10:36:41 -0800

    bug fixed

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Container, Flex, Heading } from "theme-ui";
 import netlifyIdentity from "netlify-identity-widget";
 
-netlifyIdentity.init({});
-
-export default (props) => (
-  <Container>
-    <Flex sx={{ flexDirection: "column", padding: 3 }}>
-      <Heading as="h1">Hello Serverless</Heading>
-      <Button
-        sx={{
-          marginTop: 3,
-          outline: "none",
-        }}
-        onClick={() => {
-          netlifyIdentity.open();
-        }}
-      >
-        LogIn
-      </Button>
-    </Flex>
-  </Container>
-);
+export default (props) => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Hello Serverless</Heading>
+        <Button
+          sx={{
+            marginTop: 3,
+            outline: "none",
+          }}
+          onClick={() => {
+            console.log(netlifyIdentity.currentUser());
+            netlifyIdentity.open();
+          }}
+        >
+          LogIn
+        </Button>
+      </Flex>
+    </Container>
+  );
+};

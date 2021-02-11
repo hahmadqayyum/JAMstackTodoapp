ea124a88793ff86bda96f0c3bdfc572394ac190f
hahmadqayyum <hahmadqayyum@gmail.com>
2021-02-11 11:38:06 -0800

    apollo server graphql reducer etc

---
+++ functions/graphql/index.js
@@ -0,0 +1,21 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolver = {
+  Query: {
+    hello: () => "Hello World",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolver,
+
+  playground: true,
+  intrespection: true,
+});
